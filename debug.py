ab5849f82dffb65df8b704e9e4d97904519eb19b
evalcony <evalcony@163.com>
2023-09-07 16:58:24 +0800

    commit

--- debug.py
+++ debug.py
@@ -2,12 +2,12 @@ import main
 import utils
 
 if __name__ == '__main__':
-    # file = 'QUEST3.TRA'
-    # res = main.convert('tra/'+file, main.Solver(mode = 'debug'))
-    # for r in res:
-    #     print(r)
-    # utils.write_file('', file, res, 'gb18030')    
+    file = 'QUEST3.TRA'
+    res = main.convert('tra/'+file, main.Solver(mode = ''))
+    for r in res:
+        print(r)
+    utils.write_file('', file, res, 'gb18030')    
 
-    line = "Moan, rff. Grr."
-    res = main.Solver('debug').solve(line)
-    print(res)
+    # line = "Moan, rff. Grr."
+    # res = main.Solver('debug').solve(line)
+    # print(res)
