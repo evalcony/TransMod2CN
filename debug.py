38bfc420e20567536e17e2d7e4f5e1fe0b78cc6d
evalcony <evalcony@163.com>
2023-09-09 12:44:25 +0800

    commit

--- debug.py
+++ debug.py
@@ -4,10 +4,10 @@ import utils
 
 def test_line():
     # line = "Wilson, I want to know why we're here. I never let anyone use some... bearomancy on me, taking the group away from Sendai and her enclave!"
-    line = "Actually, I was hoping to talk to—what's his name again?—Coran"
+    line = "Duration: 1 turn + 1 round/level"
 
     # 如果设置mode='debug'，则不会调用API，而且也会打印更详细的信息，而且不会有time.wait()
-    res = main.Solver(mode='debug').solve(line)
+    res = main.Solver(mode='').solve(line)
     print(res)
 
 def to_upper():
