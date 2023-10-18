9902d8d20437d99afb6b167170efa38ac4808d3f
evalcony <evalcony@163.com>
2023-10-18 13:08:21 +0800

    文件id范围比对

---
+++ tools/id_check.py
@@ -0,0 +1,39 @@
+import sys
+
+sys.path.append("..")
+import utils
+
+
+def work():
+    en_file_list = []
+    zh_file_list = []
+    for i in range(1, 96):
+        # 英文源文件
+        en_file_list.append('output/orig/' + 'sod_' + str(i) + '_orig.tra')
+        # 汉化文件
+        zh_file_list.append('output/' + 'sod_' + str(i) + '.tra')
+
+    res = []
+
+    for i in range(len(zh_file_list)):
+        zh_file = zh_file_list[i]
+        en_file = en_file_list[i]
+
+        zh_lines = utils.read_file(zh_file)
+        en_lines = utils.read_file(en_file)
+
+        res.append(zh_file)
+        res.append(zh_lines[0])
+        res.append(en_lines[0])
+        res.append('')
+        res.append(zh_lines[-1])
+        res.append(en_lines[-1])
+        res.append('-'*20)
+
+    utils.write_file('', 'id_check.txt', res)
+    print('校验完成')
+
+# 用于进行文件的比对
+# 比较2个文件的行首、行尾，确保两个文件的的范围一致
+if __name__ == '__main__':
+    work()
