0a3608fe4c09e42c71404dd46e8a0b3de860a9ba
evalcony <evalcony@163.com>
2023-11-08 18:05:43 +0800

    fix

--- init_shell.sh
+++ init_shell.sh
@@ -6,6 +6,13 @@ echo "输入namespace。若无则直接按回车(enter)"
 # 读取用户输入
 read NAMESPACE
 
+# resource 目录
+if [ ! -d resource ]; then
+    mkdir resource
+fi
+
+cd resource
+
 # 判断用户输入参数
 if [ -n "$NAMESPACE" ]; then
   # 用户输入不为空，则存入NAMESPACE变量中
@@ -16,13 +23,6 @@ if [ -n "$NAMESPACE" ]; then
   cd "$NAMESPACE"
 fi
 
-# resource 目录
-if [ ! -d resource ]; then
-    mkdir resource
-fi
-
-cd resource
-
 echo '创建 tra/'
 mkdir tra
 
@@ -32,26 +32,26 @@ mkdir output
 echo '创建 temp_dir/'
 mkdir temp_dir
 
-echo '删除并创建 readlog.txxt'
+echo '删除并创建 readlog.txt'
 del readlog.txt
 touch readlog.txt
 
+if [ -n "$NAMESPACE" ]; then
+  cd ..
+fi
+
 cd ..
-
-echo '拷贝 appconf_demo.int 到 appconf.ini'
-cp appconf_demo.ini appconf.ini
-
-# 指定要修改的文件
-FILE=appconf.ini
 
 # 指定要追加的内容
 APPEND=$NAMESPACE
 echo 'APPEND='$NAMESPACE
 
 # 指定要修改的文件
-FILE=appconf.ini
+FILE=appconf_demo.ini
 
 # 使用 sed 命令更新目标行内容
-sed -i -e 's/namespace=/namespace='$APPEND'/g' $FILE
+sed -e 's/namespace=/namespace='$APPEND'/g' $FILE > appconf.ini
+
+echo '生成appconf.ini'
 
 echo '脚本执行结束'
