b96cbe34bbd9a0157c17125c457f9c06c9e11005
evalcony <evalcony@163.com>
2023-11-25 03:30:21 +0800

    一些更新

--- comp_sh.sh
+++ comp_sh.sh
@@ -2,7 +2,25 @@
 
 FILENAME=$1
 
-NAMESPACE=tsujatha
+# ------------------- 获取 namespace
+# 读取配置文件
+conf_file="appconf.ini"
+
+# 获取行号
+line_num=$(grep -n "namespace =" "$conf_file" | cut -d ":" -f 1)
+
+# 读取指定行
+line=$(sed -n "$line_num"p "$conf_file")
+
+# 获取`=`后面的内容
+namespace=$(echo "$line" | cut -d "=" -f 2)
+
+# 输出结果
+echo "namespace: $namespace"
+
+NAMESPACE=$namespace
+
+# ---------------- 处理相关文件
 
 cp resource/$NAMESPACE/v10/$FILENAME resource/$NAMESPACE/comp/_$FILENAME
 cp resource/$NAMESPACE/output/$FILENAME resource/$NAMESPACE/comp/$FILENAME
